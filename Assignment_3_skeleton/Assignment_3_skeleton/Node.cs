6918b7d022dd67a2eb35f4da818e36c5deff78fe
Adan HZ <153233028+4d4n-HDZ@users.noreply.github.com>
2025-08-07 16:29:41 -0600

    Main Structure of the file

---
+++ Assignment_3_skeleton/Assignment_3_skeleton/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_skeleton
+{
+    internal class Node
+    {
+        object data;
+        Node next;
+
+        public object Data { get => data; set => data = value; }
+        public Node Next { get => next; set => next = value; }
+
+        public Node(object obj, Node node)
+        {
+            data = obj;
+            next = node;
+        }
+    }
+}
